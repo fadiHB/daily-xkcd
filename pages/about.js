fdd8ad237bece1196dd099a312c1300985516253
fadiHB <eng.fadi.hb@gmail.com>
2021-01-20 16:26:16 +0200

    part 1

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+
+function About () {
+    return(
+
+        <>
+
+           <h1> Welcome to about page </h1> 
+        </>
+
+
+    )
+
+
+}
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
 import styles from '../styles.module.css'
+import About from './about'
+import Link from 'next/link'
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <div className={styles.hello}>
-      <p>Hello World</p>
-    </div>
+    <>
+
+      <Link href = '/'>
+        <a>Home</a>
+      </Link>
+      <Link href = './about'>
+        <a>About</a>
+      </Link>
+      <div className={styles.hello}>
+        <p>Hello World</p>
+      </div>
+
+    </>
+
   )
+}
+
+export async function getServerSideProps(){
+  const response = await fetch('https://xkcd.com/info.0.json');
+  const jsonData  = await response.json();
+  console.log(jsonData)
+  return {props: {users: jsonData}}
 }
